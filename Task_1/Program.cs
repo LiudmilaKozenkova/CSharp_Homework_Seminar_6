2b5795571776ccb6073a7c39c1dfca1a2f974c1b
Liudmila <to_shagalova_lu@mail.ru>
2022-10-07 18:01:25 +0500

    task 1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41.
+// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223 -> 3
+
+Console.WriteLine("Введите несколько чисел через пробел: ");
+
+string line = Console.ReadLine();
+string[] elements = line.Split(' ');
+int[] numbers = Array.ConvertAll(elements, int.Parse);
+
+int count = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+        count = count + 1;
+}
+
+Console.WriteLine(count);
+
